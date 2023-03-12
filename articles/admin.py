19ac29390b4301d18cd5230bbb69cb218a4db938
Ali Ganji <aliganji142879@gmail.com>
2023-03-13 00:08:46 +0330

    add app comment

--- articles/admin.py
+++ articles/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Comment
+
+
+class CommentInline(admin.StackedInline):
+    model = Comment
 
 
 @admin.register(Article)
-class Article(admin.ModelAdmin):
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInline, ]
     list_display = ("title", "author", "data",)
 
+
+@admin.register(Comment)
+class AdminComment(admin.ModelAdmin):
+    list_display = ("article", "comment", "author",)
